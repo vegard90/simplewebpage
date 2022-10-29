f6ce1675ae43d3f2b153542da48ef8087ca06169
vegard90 <116950063+vegard90@users.noreply.github.com>
2022-10-29 15:48:31 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/simplewebpage/" : "/",
+    publicPath: process.env.NODE_ENV !== "development" ? "/simplewebpage/" : "/",
 };
